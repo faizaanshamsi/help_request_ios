56b8297f64c09f37fa6b81fb5a687999d6d43de3
faizaanshamsi <faizaan.shamsi@thinkfit.me>
2014-06-28 13:58:16 -0400

    disable csrf

--- Gemfile
+++ Gemfile
@@ -3,4 +3,6 @@ source 'https://rubygems.org'
 gem 'rake'
 # Add your dependencies here:
 gem 'afmotion', '~> 2.1.0'
-gem 'ProMotion'
+gem 'ProMotion', :git => 'https://github.com/clearsightstudio/ProMotion.git'
+gem 'formotion', :git => 'https://github.com/clayallsopp/formotion.git'
+gem 'ProMotion-formotion'
---
+++ app/screens/new_help_request.rb
@@ -0,0 +1,60 @@
+class NewHelpRequest < PM::FormotionScreen
+  def table_data
+    {
+      sections: [{
+        title: 'Submit a Help Request',
+        rows: [
+        {
+          title: "Title",
+          key: :title,
+          type: :string,
+          placeholder: 'Quick Question',
+          auto_correction: :no,
+          auto_capitalization: :none,
+          input_accessory: :done
+        },
+        {
+          title: "Description",
+          key: :description,
+          type: :text,
+          placeholder: 'This is a really quick question',
+          row_height: 100,
+          auto_correction: :no,
+          auto_capitalization: :none,
+          input_accessory: :done
+        },
+        {
+          title: 'Submit',
+          type: :submit
+        }]
+      }]
+    }
+  end
+
+  def on_submit(_form)
+    data = _form.render
+    request_body = build_request(data)
+    post(request_body)
+    open HelpRequests.new(nav_bar: true)
+  end
+
+  def build_request(data)
+    help = {
+      help: {
+        title: data[:title],
+        description: data[:description],
+        status: "Open"
+      }
+    }
+  end
+
+  def post(help)
+    AFMotion::JSON.post("http://ember-help.herokuapp.com/api/v1/helps", help) do |result|
+      if result.success?
+        p result.object
+      else
+        App.alert(result.error.localizedDescription)
+      end
+    end
+  end
+end
